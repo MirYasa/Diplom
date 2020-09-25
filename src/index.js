4b923f5e8ad9a30ead1b56756b8c541f9326f7b2
Miron Gurevich <gurevich.miron14@gmail.com>
2020-09-25 17:30:15 +0300

    feat (fixedBlocks) create function FixedBlocks

--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@ import mainSlider from './modules/mainSlider';
 import gallerySlider from './modules/gallerySlider';
 import carouselSlider from './modules/carouselSlider';
 import calculator from './modules/calculator';
+import fixedBlocks from './modules/fixedBlocks';
 
+
+fixedBlocks();
 calculator();
 carouselSlider();
 gallerySlider();
---
+++ src/modules/fixedBlocks.js
@@ -0,0 +1,24 @@
+const fixedBlocks = () => {
+    const totop = document.querySelector('#totop'),
+        menuButton = document.querySelector('.menu-button'),
+        topMenu = document.querySelector('.top-menu'),
+        headSlider = document.querySelector('.head-slider');
+
+    totop.style.display = 'none';
+    document.addEventListener('scroll', () => {
+
+        if (headSlider.getBoundingClientRect().y < -400) {
+            totop.style.display = 'block';
+        } else {
+            totop.style.display = 'none';
+        }
+
+        if (menuButton.getBoundingClientRect().y < 0) {
+            topMenu.style.position = 'fixed';
+        }
+        if (headSlider.getBoundingClientRect().y > 0) {
+            topMenu.style.position = 'relative';
+        }
+    });
+};
+export default fixedBlocks;
